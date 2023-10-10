857650024b5c1432c94c8396f5356ac7a7e14063
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2023-10-10 10:08:22 +0200

    Class Coordenada Create

---
+++ Proyectos/Juego_Parejas_santi/src/Coordenada.java
@@ -0,0 +1,33 @@
+
+public class Coordenada {
+	private Integer ejeX;
+	private Integer ejeY;
+	
+	public Coordenada(Integer ejeX, Integer ejeY) {
+		super();
+		this.ejeX = ejeX;
+		this.ejeY = ejeY;
+	}
+	public Coordenada() {
+		super();
+		this.ejeX = 0;
+		this.ejeY = 0;
+	}
+	
+	public Integer getEjeX() {
+		return ejeX;
+	}
+	public Integer getEjeY() {
+		return ejeY;
+	}
+
+	private void setEjeX(Integer ejeX) {
+		this.ejeX = ejeX;
+	}
+
+	private void setEjeY(Integer ejeY) {
+		this.ejeY = ejeY;
+	}
+	
+	
+}
